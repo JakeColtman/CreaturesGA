865f58e34cb329f2b2b7ce9e4d96502c677a8d89
Jake <Jake Coltman>
2016-08-17 09:20:42 +0100

    Tightened up end point

--- Market/end_point.py
+++ Market/end_point.py
@@ -1,16 +1,43 @@
 import flask
 import redis 
 import json
+from flask import request
+from message_pb2 import Message
+from kafka import KafkaProducer
 
 app = flask.Flask(__name__)
 
-
-@app.route('/resource/<int:user_id>', methods=['GET'])
-def home(user_id):
+@app.route('/market/user/<int:user_id>', methods=['GET'])
+def user(user_id):
     amount = conn.get("user:" + str(user_id))
-    return json.dumps({"id": user_id, "amount": int(amount)})
+    print(amount)
+    try:
+        return json.dumps({"id": user_id, "amount": int(amount)})
+    except TypeError:
+        return json.dumps({"id": user_id, "amount": 0})
+
+@app.route('/market/trade', methods=['POST'])
+def trade():
+    body = request.json
+    user_a, user_b = body["user_a_id"], body["user_b_id"]
+    print(user_a, user_b)
+    amount = body["amount"]
+
+    conn.incr("user:" + str(user_a), int(amount))
+    conn.decr("user:" + str(user_b), int(amount))
+
+    comm = Message()
+    comm.trade.user_a_id = user_a
+    comm.trade.user_b_id = user_b
+    comm.trade.commodity_id = 1
+    comm.trade.amount = amount
+
+    producer.send("resource", comm.SerializeToString())
+
+    return json.dumps({"status": "Success"})
 
 
 if __name__ == '__main__':
    conn = redis.StrictRedis(host = 'localhost', port=6379)  
+   producer = KafkaProducer(bootstrap_servers=['0.0.0.0:9092'])
    app.run(debug=True)
---
+++ Market/example_request.py
@@ -0,0 +1,9 @@
+import requests
+import json
+data = {
+    "user_a_id": 1,
+    "user_b_id": 2,
+    "amount": 100
+}
+
+print(requests.post("http://127.0.0.1:5000/market/trade", data = json.dumps(data), headers={"Content-Type": "application/json"}))
